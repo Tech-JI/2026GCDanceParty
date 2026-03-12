3c1b606eb01cc3a08c3aa24970690f25dbe95eef
lucky <luckysugar@sjtu.edu.cn>
2026-03-13 00:46:06 +0800

    feat: registration

--- miniprogram/pages/edit/edit.js
+++ miniprogram/pages/edit/edit.js
@@ -1,19 +1,20 @@
-// edit页面时点入个人界面后点击编辑个人信息跳出的页面
-const db = wx.cloud.database(); // 获得数据库
+﻿const db = wx.cloud.database(); 
 
 Page({
   data: {
-    openid: '', // 用户的openid
-    dataobj: {}, // 用户自己的数据 record
-    done: false, // 页面 数据库 是否加载完成的bool
-    messagelove: '', // 用户填写的 “我喜欢的” 的字符串
-    lovelength: 0, // messagelove的字数，用于设置字数限制
+    openid: '', 
+    dataobj: {}, 
+    done: false, 
+    messagelove: '', 
+    lovelength: 0, 
     nicknamelength: 0,
-    messagelength: 0, // 用户填写的 “个性名片” 的字数限制
-    messagemsg: '', // 用户填写的 “个性名片” 的字符串
+    messagelength: 0, 
+    messagemsg: '', 
     messagenickname: '',
     avatarUrl: "",
     upload_file: false,
+    selfieUrl: "",
+    upload_selfie: false,
     messageschool: '',
     genderOptions: ['男', '女'],
     genderIndex: -1,
@@ -27,6 +28,19 @@ Page({
       avatarUrl: avatarUrl,
       upload_file: true
     })
+  },
+  onChooseSelfie() {
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        this.setData({
+          selfieUrl: res.tempFiles[0].tempFilePath,
+          upload_selfie: true
+        });
+      }
+    });
   },
   inputschool(e) {
     this.setData({ messageschool: e.detail.value });
@@ -61,45 +75,32 @@ Page({
     });
   },
 
-  // 输入框触发的函数 用于储存用户输入内容的函数
   inputnickname(e) {
     var msg = e.detail.value;
-    this.setData({
-      nicknamelength: msg.length
-    });
+    this.setData({ nicknamelength: msg.length });
     if (msg.length <= 30) {
-      this.setData({
-        messagenickname: msg,
-      });
+      this.setData({ messagenickname: msg });
     }
   },
 
-  // 输入框触发的函数 用于储存用户输入内容的函数
   inputlove(e) {
     var msg = e.detail.value;
-    this.setData({
-      lovelength: msg.length
-    });
+    this.setData({ lovelength: msg.length });
     if (msg.length <= 30) {
-      this.setData({
-        messagelove: msg,
-      });
-    }
-  },
-  textcheck(e) {
-    var msg = e.detail.value;
-    this.setData({
-      messagelength: msg.length
-    });
-    if (msg.length <= 400) {
-      this.setData({
-        messagemsg: msg
-      })
+      this.setData({ messagelove: msg });
     }
   },
 
-  // 提交按钮，确认编辑的内容
-  btnSub(e) {
+  textcheck(e) {
+    var msg = e.detail.value;
+    this.setData({ messagelength: msg.length });
+    if (msg.length <= 400) {
+      this.setData({ messagemsg: msg })
+    }
+  },
+
+  async btnSub(e) {
+    wx.showLoading({ title: '保存中' });
     const openid = wx.getStorageSync('openid');
     const accountUserId = wx.getStorageSync('accountUserId');
     const updateCurrentUser = (data) => {
@@ -109,75 +110,58 @@ Page({
       return db.collection('ActiveUser').where({ _openid: openid }).update({ data });
     };
 
-    if (this.data.upload_file) {
-      wx.cloud.uploadFile({
-        cloudPath: (openid || accountUserId || Date.now()) + ".jpg",
-        filePath: this.data.avatarUrl, // 文件路径
-        success: res => {
-          this.setData({
-            user_avatar: res.fileID,
-            avatarUrl: res.fileID
-          })
-          console.log(e);
-          var newgrade = e.detail.value.editgrade;
-          var newlove = this.data.messagelove;
-          var newmessage = this.data.messagemsg;
-          var newnickname = this.data.messagenickname;
-          console.log(newmessage);
-          updateCurrentUser({
-            grade: newgrade,
-            love: newlove,
-            message: newmessage,
-            nickname: newnickname,
-            school: this.data.messageschool,
-            gender: this.data.selectedGender,
-            image: res.fileID
-          }).then((res) => {
-            console.log(res)
-          })
-          wx.switchTab({
-            url: '../mine/mine',
-          })
-        }
-      })
-    }
-    else {
-      var newgrade = e.detail.value.editgrade;
-      var newlove = this.data.messagelove;
-      var newmessage = this.data.messagemsg;
-      var newnickname = this.data.messagenickname;
-      console.log(newmessage);
-      updateCurrentUser({
-        grade: newgrade,
-        love: newlove,
-        message: newmessage,
-        nickname: newnickname,
-        school: this.data.messageschool,
-        gender: this.data.selectedGender,
-      }).then((res) => {
-        console.log('✅ 用户信息更新成功:', res);
-      })
-      wx.switchTab({
-        url: '../mine/mine',
-      })
+    var newgrade = e.detail.value.editgrade;
+    var newlove = this.data.messagelove;
+    var newmessage = this.data.messagemsg;
+    var newnickname = this.data.messagenickname;
+    
+    let updateData = {
+      grade: newgrade,
+      love: newlove,
+      message: newmessage,
+      nickname: newnickname,
+      school: this.data.messageschool,
+      gender: this.data.selectedGender,
+    };
+
+    try {
+      if (this.data.upload_file) {
+        const res = await wx.cloud.uploadFile({
+          cloudPath: 'avatars/' + (openid || accountUserId || Date.now()) + '_' + Date.now() + '.jpg',
+          filePath: this.data.avatarUrl
+        });
+        updateData.image = res.fileID;
+      }
+      
+      if (this.data.upload_selfie) {
+        const res = await wx.cloud.uploadFile({
+          cloudPath: 'selfies/' + (openid || accountUserId || Date.now()) + '_' + Date.now() + '.jpg',
+          filePath: this.data.selfieUrl
+        });
+        updateData.selfieImage = res.fileID;
+      }
+
+      await updateCurrentUser(updateData);
+      wx.hideLoading();
+      wx.switchTab({ url: '../mine/mine' });
+
+    } catch(err) {
+      console.error(err);
+      wx.hideLoading();
+      wx.showToast({ title: '保存失败', icon: 'error' });
     }
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
 
-  // 获得用户的openid
   get_openid() {
     const accountUserId = wx.getStorageSync('accountUserId');
-    this.setData({
-      done: false
-    });
+    this.setData({ done: false });
     const onLoadCurrentUser = (userRecord) => {
       const genderOptions = ['男', '女'];
-      const genderIndex = genderOptions.indexOf(userRecord.gender || '');
+      const genderIndex = genderOptions.indexOf(userRecord.gender || '');       
       this.setData({
         dataobj: userRecord,
         avatarUrl: userRecord.image,
+        selfieUrl: userRecord.selfieImage || '',
         done: true,
         lovelength: (userRecord.love || '').length,
         messagelove: userRecord.love || '',
@@ -192,29 +176,22 @@ Page({
     if (accountUserId) {
       db.collection("ActiveUser").doc(accountUserId).get({
         success: res => {
-          if (!res.data) {
-            return;
-          }
+          if (!res.data) return;
           onLoadCurrentUser(res.data);
         }
       });
       return;
     }
 
-    // 从数据库中找到用户的信息
     db.collection("ActiveUser").where({ _openid: wx.getStorageSync('openid') }).get({
       success: res => {
-        if (!res.data.length) {
-          return;
-        }
+        if (!res.data.length) return;
         onLoadCurrentUser(res.data[0]);
       }
     });
 
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  
   onLoad: function (options) {
     this.get_openid();
   },
--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,47 +1,80 @@
-// 登陆页面
-const db = wx.cloud.database();
+﻿const db = wx.cloud.database();
 const _ = db.command;
 Page({
   data: {
-    authority: false, // 用户是否微信授权头像和昵称，不授权不能来玩！
-    // 海报的图片 - 使用本地图片或CDN
-    poster: "/icons/invite.png", 
-    imageUrl: "/icons/invite.png",
-    userInfo: {}, // 用户填写的信息
-    name: "", // 用户填入的姓名
-    openid: '', // 用户的openid
-    haslog: false, // 用户是否已经登陆过了
-    done: false, // 页面和数据库是否加载完毕,
+    isLoginMode: true, // 默认登录模式
+    authority: false,
+    poster: '/icons/invite.png', 
+    imageUrl: '/icons/invite.png',
+    userInfo: {},
+    name: '',
+    openid: '',
+    haslog: false,
+    done: false,
     submit_status: false,
-    
     theme: wx.getSystemSetting().theme,
-    user_avatar: "",
-    avatarUrl: "/icons/person.png", // 默认头像
+    user_avatar: '',
+    avatarUrl: '/icons/person.png',
     avatar_submit: false,
-    debug: "initial state",
+    debug: 'initial state',
     count: 0,
     onstack: false
+  },
+
+  switchMode(e) {
+    const mode = e.currentTarget.dataset.mode;
+    this.setData({
+      isLoginMode: mode === 'login'
+    });
   },
 
   async btnSub(e) {
     if (this.data.count !== 0 || this.data.onstack === true) {
       return;
     }
-
     this.setData({ onstack: true });
 
-    const nickname = (e.detail.value.nickname || '').trim();
-    const userid = (e.detail.value.userid || '').trim();
-    const password = (e.detail.value.password || '').trim();
+    if (this.data.isLoginMode) {
+      await this.handleLogin(e);
+    } else {
+      await this.handleRegister(e);
+    }
+  },
 
-    if (!userid) {
-      wx.showToast({ title: '请输入姓名', icon: 'error' });
+  async handleRegister(e) {
+    const realname = (e.detail.value.realname || '').trim();
+    const phone = (e.detail.value.phone || '').trim();
+    const account = (e.detail.value.account || '').trim();
+    const password = (e.detail.value.password || '').trim();
+    const confirmPassword = (e.detail.value.confirmPassword || '').trim();
+
+    if (this.data.avatar_submit === false) {
+      wx.showToast({ title: '请上传头像', icon: 'none' });
       this.setData({ onstack: false });
       return;
     }
-
-    if (!password || !/^1[3-9]\d{9}$/.test(password)) {
-      wx.showToast({ title: '请输入正确的手机号', icon: 'error' });
+    if (!realname) {
+      wx.showToast({ title: '请输入真实姓名', icon: 'none' });
+      this.setData({ onstack: false });
+      return;
+    }
+    if (!phone || !/^1[3-9]\d{9}$/.test(phone)) {
+      wx.showToast({ title: '请输入正确的手机号', icon: 'none' });
+      this.setData({ onstack: false });
+      return;
+    }
+    if (!account) {
+      wx.showToast({ title: '请输入注册账号', icon: 'none' });
+      this.setData({ onstack: false });
+      return;
+    }
+    if (!password) {
+      wx.showToast({ title: '请输入密码', icon: 'none' });
+      this.setData({ onstack: false });
+      return;
+    }
+    if (password !== confirmPassword) {
+      wx.showToast({ title: '两次密码不一致', icon: 'none' });
       this.setData({ onstack: false });
       return;
     }
@@ -51,173 +84,167 @@ Page({
       const openidResult = (openidRes && openidRes.result) ? openidRes.result : {};
       const wxOpenid = openidResult.openid || openidResult.openId || '';
       if (!wxOpenid) {
-        wx.showToast({ title: '微信身份获取失败', icon: 'error' });
+        wx.showToast({ title: '微信身份获取失败', icon: 'none' });
         this.setData({ onstack: false });
         return;
       }
-
       wx.setStorageSync('wxOpenid', wxOpenid);
 
-      const userResult = await db.collection('User').where({
-        name: userid,
-        phone: password
+      const userResult = await db.collection('ActiveUser').where({
+        _openid: wxOpenid
       }).get();
+      
+      const accountResult = await db.collection('ActiveUser').where(_.or([
+        { account: account },
+        { phone: phone }
+      ])).get();
 
-      if (!userResult.data.length) {
-        wx.showToast({ title: '用户名或手机号错误', icon: 'error' });
+      if (accountResult.data.length > 0) {
+        wx.showToast({ title: '该账号或手机号已被注册', icon: 'none' });
         this.setData({ onstack: false });
         return;
       }
 
-      const person = userResult.data[0];
+      const uploadResult = await wx.cloud.uploadFile({
+        cloudPath: 'avatars/' + account + '_' + Date.now() + '.png',
+        filePath: this.data.user_avatar
+      });
+      const avatarFileId = uploadResult.fileID;
 
-      let activeResult = await db.collection('ActiveUser').where({ accountId: person._id }).get();
-      if (!activeResult.data.length) {
-        activeResult = await db.collection('ActiveUser').where({
-          name: userid,
-          phone: password
-        }).get();
-      }
+      const randomNumericId = Date.now() % 1000000;
+      const fakeAccountId = 'user_' + Date.now();
 
-      let activeUser = null;
-
-      if (!activeResult.data.length) {
-        if (this.data.avatar_submit === false) {
-          wx.showToast({ title: '请上传头像', icon: 'error', mask: true });
-          this.setData({ onstack: false });
-          return;
-        }
-
-        if (!nickname) {
-          wx.showToast({ title: '请填写昵称', icon: 'error', mask: true });
-          this.setData({ onstack: false });
-          return;
-        }
-
-        const uploadResult = await wx.cloud.uploadFile({
-          cloudPath: `avatars/${userid}_${Date.now()}.png`,
-          filePath: this.data.user_avatar
-        });
-
-        const avatarFileId = uploadResult.fileID;
-        const addResult = await db.collection('ActiveUser').add({
-          data: {
-            accountId: person._id,
-            wxOpenid: wxOpenid,
-            name: person.name,
-            phone: person.phone,
-            receive: {},
-            send: {},
-            gender: '',
-            school: '',
-            grade: '',
+      const addResult = await db.collection('ActiveUser').add({
+        data: {
+          accountId: fakeAccountId, 
+          account: account,
+          password: password, 
+          wxOpenid: wxOpenid,
+          name: realname,
+          phone: phone,
+          numericId: randomNumericId,
+          receive: {},
+          send: {},
+          gender: '',
+          school: '',
+          grade: '',
+          canedit: true,
+          love: '',
+          message: '',
+          partner: {
+            hasPartner: false,
+            partnerName: '',
+            partnerAccountUserId: '',
+            partnerNumericId: '',
+            matchTime: null,
+            matchType: '',
+            allowRandomMatch: false
+          },
+          nickname: realname, // 未填写微信昵称时默认使用真实姓名作昵称
+          image: avatarFileId,
+          profile: {
+            title: '',
+            signature: '',
+            avatar: avatarFileId,
+            isPublic: true,
+            completed: true
+          },
+          privacy: {
+            allowSearch: true,
+            allowInvitation: true
+          },
+          registration: {
+            registered: false,
+            paymentStatus: 'unpaid'
+          },
+          settings: {
+            language: 'zh',
             canedit: true,
-            love: '',
-            message: '',
-            partner: {
-              hasPartner: false,
-              partnerName: '',
-              partnerAccountUserId: '',
-              partnerNumericId: '',
-              matchTime: null,
-              matchType: '',
-              allowRandomMatch: false
-            },
-            nickname: nickname,
-            image: avatarFileId
+            lastLoginTime: new Date()
           }
-        });
-
-        const tempNumericId = Date.now() % 1000000;
-        await db.collection('ActiveUser').doc(addResult._id).update({
-          data: {
-            numericId: tempNumericId,
-            profile: {
-              title: '',
-              signature: '',
-              avatar: avatarFileId,
-              isPublic: true,
-              completed: true
-            },
-            privacy: {
-              allowSearch: true,
-              allowInvitation: true,
-              showSchool: true,
-              showMajor: true,
-              showGrade: true,
-              showExperience: true,
-              showAvatar: true,
-              showSignature: true
-            },
-            registration: {
-              registered: false,
-              hasDiscount: false,
-              discountProof: '',
-              discountStatus: 'pending',
-              paymentStatus: 'unpaid',
-              orderId: '',
-              paymentTime: null,
-              qrCodeShown: false
-            },
-            settings: {
-              language: 'zh',
-              canedit: true,
-              lastLoginTime: new Date()
-            }
-          }
-        });
-
-        const createdUserResult = await db.collection('ActiveUser').doc(addResult._id).get();
-        activeUser = createdUserResult.data;
-
-        wx.showToast({ title: '注册成功', icon: 'success' });
-      } else {
-        activeUser = activeResult.data[0];
-
-        await db.collection('ActiveUser').doc(activeUser._id).update({
-          data: {
-            accountId: person._id,
-            wxOpenid: wxOpenid,
-            'settings.lastLoginTime': new Date()
-          }
-        });
-
-        const isProfileComplete = activeUser.name && activeUser.phone && activeUser.nickname;
-        if (isProfileComplete && (!activeUser.profile || !activeUser.profile.completed)) {
-          await db.collection('ActiveUser').doc(activeUser._id).update({
-            data: {
-              'profile.completed': true,
-              'profile.isPublic': true
-            }
-          });
         }
-
-        wx.showToast({ title: '登录成功', icon: 'success' });
-      }
-
-      wx.setStorageSync('openid', activeUser._openid);
-      wx.setStorageSync('accountUserId', activeUser._id);
-      wx.setStorageSync('accountId', person._id);
-      wx.setStorageSync('isLoggedIn', true);
-      wx.setStorageSync('userInfo', {
-        name: activeUser.name,
-        phone: activeUser.phone,
-        numericId: activeUser.numericId,
-        nickname: activeUser.nickname,
-        hasPartner: !!(activeUser.partner && activeUser.partner.hasPartner)
       });
 
-      this.setData({
-        onstack: false,
-        count: 1
-      });
-
-      wx.switchTab({ url: '../home/home' });
+      wx.showToast({ title: '注册成功！', icon: 'success' });
+      const createdUserResult = await db.collection('ActiveUser').doc(addResult._id).get();
+      this.saveLoginState(createdUserResult.data);
+      
     } catch (err) {
-      console.error('❌ 登录流程失败:', err);
-      wx.showToast({ title: '登录失败，请重试', icon: 'error' });
+      console.error('注册失败:', err);
+      wx.showToast({ title: '注册失败', icon: 'none' });
       this.setData({ onstack: false });
     }
+  },
+
+  async handleLogin(e) {
+    const account = (e.detail.value.account || '').trim();
+    const password = (e.detail.value.password || '').trim();
+
+    if (!account || !password) {
+      wx.showToast({ title: '请输入账号和密码', icon: 'none' });
+      this.setData({ onstack: false });
+      return;
+    }
+
+    try {
+      const userResult = await db.collection('ActiveUser').where({
+        account: account,
+        password: password
+      }).get();
+
+      if (userResult.data.length === 0) {
+        wx.showToast({ title: '账号或密码错误', icon: 'none' });
+        this.setData({ onstack: false });
+        return;
+      }
+
+      const activeUser = userResult.data[0];
+
+      await db.collection('ActiveUser').doc(activeUser._id).update({
+        data: {
+          'settings.lastLoginTime': new Date()
+        }
+      });
+
+      wx.showToast({ title: '登录成功', icon: 'success' });
+      this.saveLoginState(activeUser);
+
+    } catch (err) {
+      console.error('登录失败:', err);
+      wx.showToast({ title: '登录失败，请重试', icon: 'none' });
+      this.setData({ onstack: false });
+    }
+  },
+
+  saveLoginState(activeUser) {
+    const app = getApp();
+    const userOpenid = activeUser.wxOpenid || activeUser._openid;
+    
+    wx.setStorageSync('openid', userOpenid); 
+    wx.setStorageSync('accountUserId', activeUser._id);
+    wx.setStorageSync('accountId', activeUser.accountId);
+    wx.setStorageSync('isLoggedIn', true);
+    wx.setStorageSync('userInfo', {
+      name: activeUser.name,
+      phone: activeUser.phone,
+      numericId: activeUser.numericId,
+      nickname: activeUser.nickname,
+      hasPartner: !!(activeUser.partner && activeUser.partner.hasPartner)
+    });
+
+    if(app) {
+      app.globalData.openid = userOpenid;
+      app.globalData.userInfo = activeUser;
+    }
+
+    this.setData({
+      onstack: false,
+      count: 1
+    });
+
+    setTimeout(() => {
+      wx.switchTab({ url: '../home/home' });
+    }, 1000);
   },
 
   onChooseAvatar(e) {
@@ -225,7 +252,6 @@ Page({
     if (!avatarUrl) {
       return;
     }
-    console.log('🖼️ 选择头像:', avatarUrl);
     this.setData({
       avatarUrl: avatarUrl,
       user_avatar: avatarUrl,
@@ -234,12 +260,10 @@ Page({
   },
 
   onAvatarError(e) {
-    const errMsg = (e && e.detail && e.detail.errMsg) ? e.detail.errMsg : '';
+    const errMsg = (e && e.detail && e.detail.errMsg) ? e.detail.errMsg : '';   
     if (errMsg.includes('cancel')) {
       return;
     }
-
-    console.error('❌ 头像选择失败:', errMsg || e);
     wx.showToast({
       title: '头像选择失败',
       icon: 'none'
@@ -253,8 +277,6 @@ Page({
       wx.switchTab({ url: '../home/home' });
       return;
     }
-    this.setData({
-      done: true
-    });
+    this.setData({ done: true });
   }
 });
